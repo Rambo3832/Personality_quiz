497d24e7a3e5033e192f7f032d3af7513f0ea6b5
jhanviaggarwal <jhanviaggarwal1610@gmail.com>
2025-04-26 19:09:33 +0530

    Added some css for all pages and options for quizzes

--- frontend/src/components/QuizSelection.js
+++ frontend/src/components/QuizSelection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./QuizSelection.css"; // Import your CSS file for styling
 
 const quizTypes = [
   { id: "office", name: "Office Personality" },
